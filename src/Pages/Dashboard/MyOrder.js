2261e06987f147b61a6834a36d419469294d74ba
SadiaMou007 <sadia.bu.cse@gmail.com>
2022-05-26 20:08:05 -0700

    deploy

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -1,21 +1,20 @@
-import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const MyOrder = () => {
   const [user] = useAuthState(auth);
   const [orders, setOrders] = useState([]);
   useEffect(() => {
-    fetch(
-      `https://floating-cliffs-31659.herokuapp.com/booking?user=${user.email}`
-    )
+    const url = `https://floating-cliffs-31659.herokuapp.com/booking?user=${user.email}`;
+    console.log(url);
+    fetch(url)
       .then((res) => res.json())
       .then((data) => setOrders(data));
-  }, [orders]);
+  }, [user.email]);
+
   console.log(orders);
-  return <div>MyOrder: {orders.length}</div>;
+  return <div>MyOrder:{orders?.length} </div>;
 };
 
 export default MyOrder;
